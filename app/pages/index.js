d611257fcfcfb242649c0b548bafc2358f7a9a39
Vincent Nguyen <vpvnguyen@gmail.com>
2020-02-04 20:53:52 -0800

    test layout component

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+import Nav from '../components/Nav'
+import Layout from '../components/Layout';
 
 const Home = () => (
   <div>
     <Head>
       <title>Home</title>
       <link rel="icon" href="/favicon.ico" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     </Head>
+
+<Layout>
 
     <Nav />
 
@@ -82,6 +87,8 @@ const Home = () => (
         color: #333;
       }
     `}</style>
+    </Layout>
+
   </div>
 )
 
