a3915bbedc43f671ef4070e9ee3012705a8eda52
Vincent Nguyen <vpvnguyen@gmail.com>
2020-02-05 22:41:23 -0800

    inital login component

---
+++ app/pages/Login.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Login = () => (
+    <>
+        <div>Login</div>
+        <Link href="/Dashboard">
+            <a>dashboard</a>
+        </Link>
+    </>
+);
+
+export default Login;
