f1e67126a0bed8a2874b4119ad99341e894127d0
vpvnguyen <vpvnguyen@gmail.com>
2020-02-07 22:19:34 -0800

    create contact footer

---
+++ app/components/Footer.js
@@ -0,0 +1,17 @@
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+
+const Footer = () => (
+<footer className="footer">
+    <a href="https://github.com/vpvnguyen" target="_blank"><GitHubIcon /></a>
+    |
+    <a href="https://linkedin.com/in/vpvnguyen" target="_blank"><LinkedInIcon /></a>
+    |
+    <a href="https://github.com/vpvnguyen" target="_blank"><MailOutlineIcon /></a>
+
+    
+</footer>
+);
+
+export default Footer;
