914b317f197f2519e48201c7712f8700406da3a7
Vincent Nguyen <vpvnguyen@gmail.com>
2020-02-08 09:34:28 -0800

    added example card to render projects

--- app/pages/Projects.js
+++ app/pages/Projects.js
@@ -1,3 +1,13 @@
-const Projects = () => <div>Projects</div>;
+// get projects on initial mount
+// display projects as cards
+
+import { Card } from '@material-ui/core';
+import ExampleCard from '../components/ExampleCard';
+
+const Projects = () => (
+    <>
+        <ExampleCard />
+    </>
+);
 
 export default Projects;
