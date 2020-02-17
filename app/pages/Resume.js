4bbf1bf355b528b8222d51369b360bb43e5f5dfc
Vincent Nguyen <vpvnguyen@gmail.com>
2020-02-16 22:50:46 -0800

    mock layout

--- app/pages/Resume.js
+++ app/pages/Resume.js
@@ -1,3 +1,11 @@
-const Resume = () => <div>Resume</div>;
+const Resume = () => (
+    <>
+        <h1>Vincent Nguyen</h1>
+        <h2>Summary</h2>
+        <h2>Experience</h2>
+        <h2>Projects</h2>
+        <h2>Education</h2>
+    </>
+);
 
 export default Resume;
